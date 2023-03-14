e25f98b557cacd407149c9b0ac7d1ba897880576
jojocodeX <571943037@qq.com>
2023-03-14 16:39:54 +0800

    fix errors

--- gaia-app-boot/src/main/java/org/bravo/gaia/app/boot/i18n/I18NAutoConfiguration.java
+++ gaia-app-boot/src/main/java/org/bravo/gaia/app/boot/i18n/I18NAutoConfiguration.java
@@ -1,9 +1,12 @@
-package org.bravo.gaia.app.boot.config;
+package org.bravo.gaia.app.boot.i18n;
 
-import org.bravo.gaia.app.boot.i18n.MessageUtil;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -12,14 +15,17 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
  * @author lijian
  * @since 2021/11/29
  */
-@Configuration
-public class I18NConfig {
+
+@AutoConfigureBefore(WebMvcAutoConfiguration.class)
+@Configuration(proxyBeanMethods = false)
+public class I18NAutoConfiguration {
     @Bean
     public MessageSource messageSource() {
         return MessageUtil.getMessageSource();
     }
 
-    @Bean
+    @Bean(name = DispatcherServlet.LOCALE_RESOLVER_BEAN_NAME)
+    @ConditionalOnMissingBean
     public LocaleResolver localeResolver(){
         return new SessionLocaleResolver();
     }
